e7cd6520218449fc603b0742c4622673efb8b10d
Maya-UIT2023002 <maya.pardeshi@cumminscollege.in>
2026-02-05 21:17:50 +0530

    Added Expense Module

---
+++ app/expense/Expense.java
@@ -0,0 +1,25 @@
+
+public class Expense {
+
+    private String paidBy;
+    private double amount;
+    private String category;
+
+    public Expense(String paidBy, double amount, String category) {
+        this.paidBy = paidBy;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public String getPaidBy() {
+        return paidBy;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
