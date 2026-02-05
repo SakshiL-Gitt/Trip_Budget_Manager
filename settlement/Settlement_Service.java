60ba95684208919af1b02b4ae326f8a0a5cbcc70
SanikaPaware12 <sanika.paware@cumminscollege.in>
2026-02-05 21:59:38 +0530

    Added Settlement_Service.java

---
+++ settlement/Settlement_Service.java
@@ -0,0 +1,40 @@
+package com.tripbudget.settlement;
+
+import com.tripbudget.expense.Expense;
+import com.tripbudget.participant.Participant;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SettlementService {
+
+    public Map<String, Double> calculateBalances(
+            List<Participant> participants,
+            List<Expense> expenses) {
+
+        Map<String, Double> balanceMap = new HashMap<>();
+
+        for (Participant p : participants) {
+            balanceMap.put(p.getName(), 0.0);
+        }
+
+        double totalExpense = 0;
+        for (Expense e : expenses) {
+            totalExpense += e.getAmount();
+            balanceMap.put(
+                    e.getPaidBy(),
+                    balanceMap.get(e.getPaidBy()) + e.getAmount()
+            );
+        }
+
+        double share = totalExpense / participants.size();
+
+        for (String name : balanceMap.keySet()) {
+            balanceMap.put(name, balanceMap.get(name) - share);
+        }
+
+        return balanceMap;
+    }
+}
+
